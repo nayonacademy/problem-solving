4bab60d896f26efdf85a45e675c4277981f5fb48
Shariful Islam <si.nayon@gmail.com>
2019-07-17 16:52:42 +0600

    1012 Area

---
+++ Uri online judge/1012.cpp
@@ -0,0 +1,13 @@
+// 1012 Area
+#include <stdio.h>
+int main()
+{
+    float a, b, c;
+    scanf("%f %f %f", &a, &b, &c);
+    printf("TRIANGULO: %.3f\n", 0.5 * a * c);
+    printf("CIRCULO: %.3f\n", 3.14159 * c * c);
+    printf("TRAPEZIO: %.3f\n", 0.5 * (a + b) * c);
+    printf("QUADRADO: %.3f\n", b * b);
+    printf("RETANGULO: %.3f\n", a * b);
+    return 0;
+}
