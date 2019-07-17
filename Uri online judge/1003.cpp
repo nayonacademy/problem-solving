1fde2a23d2e74fc56cb8349f204cc1403ed388cf
Shariful Islam <si.nayon@gmail.com>
2019-07-17 16:22:52 +0600

    1003 - Simple Sum

---
+++ Uri online judge/1003.cpp
@@ -0,0 +1,11 @@
+// 1003 - Simple Sum
+
+#include <stdio.h>
+
+int main()
+{
+    int var_one, var_two, result;
+    scanf("%d %d", &var_one, &var_two);
+    printf("SOMA = %d\n", var_one + var_two);
+    return 0;
+}
