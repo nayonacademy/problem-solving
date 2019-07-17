0a60bdd60313e7f3afad306925255cbc6f396582
Shariful Islam <si.nayon@gmail.com>
2019-07-17 16:35:48 +0600

    1007 - Difference

---
+++ 1007.cpp
@@ -0,0 +1,10 @@
+// 1007 - Difference
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d, result;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    result = (a * b - c * d);
+    printf("DIFERENCA = %d\n", result);
+    return 0;
+}
