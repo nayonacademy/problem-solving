d1e9b1ea09dacb07d3a4b0e163163cb3825bf51d
Shariful Islam <si.nayon@gmail.com>
2019-07-17 17:15:09 +0600

    1043 - Triangle

---
+++ Uri online judge/1043.cpp
@@ -0,0 +1,17 @@
+//1043 - Triangle
+#include <stdio.h>
+    int
+    main()
+{
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        printf("Perimetro = %.1f\n", a + b + c);
+    }
+    else
+    {
+        printf("Area = %.1f\n", c * (a + b) / 2);
+    }
+    return 0;
+}
