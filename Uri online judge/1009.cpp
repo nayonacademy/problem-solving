bad84c85462f3464da6927a0cb156f816b946650
Shariful Islam <si.nayon@gmail.com>
2019-07-17 16:47:09 +0600

    #include <stdio.h>

---
+++ Uri online judge/1009.cpp
@@ -0,0 +1,11 @@
+//1009 - Salary with Bonus
+#include <stdio.h>
+int main()
+{
+    char name;
+    double salary, sale, sales_rev;
+    scanf("%s %lf %lf", &name, &salary, &sale);
+    sales_rev = salary + (sale * 15) / 100;
+    printf("TOTAL = R$ %.2f\n", sales_rev);
+    return 0;
+}
