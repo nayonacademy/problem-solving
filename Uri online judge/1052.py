5076492230fce6fb5e27f32a761d87c96e2c326f
Shariful Islam <si.nayon@gmail.com>
2019-07-17 17:27:41 +0600

    1052 - Month

---
+++ Uri online judge/1052.py
@@ -0,0 +1,13 @@
+# 1052 - Month
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+mystr = int(input())
+months = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+result = months[mystr-1]
+print(result)
