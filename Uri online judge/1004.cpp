d9fb489ef83b23070f1a1f9f1f646e27a0ecd2c0
Shariful Islam <si.nayon@gmail.com>
2019-07-17 16:23:31 +0600

    1004 - Simple Product

---
+++ Uri online judge/1004.cpp
@@ -0,0 +1,10 @@
+// 1004 - Simple Product
+#include <stdio.h>
+
+int main()
+{
+    int var_one, var_two, result;
+    scanf("%d %d", &var_one, &var_two);
+    printf("PROD = %d\n", var_one * var_two);
+    return 0;
+}
