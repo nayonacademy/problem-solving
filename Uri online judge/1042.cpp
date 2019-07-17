582d909f245950bf1cbe28fcb1a44f7b0cbe917d
Shariful Islam <si.nayon@gmail.com>
2019-07-17 17:13:47 +0600

    1042 - Simple Sort

---
+++ Uri online judge/1042.cpp
@@ -0,0 +1,30 @@
+// 1042 - Simple Sort
+#include <stdio.h>
+int main()
+{
+    int a, b, c, g, m, s, temp;
+    scanf("%d %d %d", &a, &b, &c);
+    g = a;
+    m = b;
+    s = c;
+    if (g < m)
+    {
+        temp = g;
+        g = m;
+        m = temp;
+    }
+    if (m < s)
+    {
+        temp = m;
+        m = s;
+        s = temp;
+    }
+    if (g < m)
+    {
+        temp = g;
+        g = m;
+        m = temp;
+    }
+    printf("%d\n%d\n%d\n\n%d\n%d\n%d\n", s, m, g, a, b, c);
+    return 0;
+}
