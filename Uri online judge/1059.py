efdc1bdbda9c6312bd0982788e27db81d5b41e1a
Shariful Islam <si.nayon@gmail.com>
2019-07-17 17:28:56 +0600

    1059 - Even Numbers

---
+++ Uri online judge/1059.py
@@ -0,0 +1,11 @@
+# 1059 - Even Numbers
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i)
