3eb2727e1e90b0ffdd22ad2bf4e461b67687adbc
Shariful Islam <si.nayon@gmail.com>
2019-07-28 00:11:58 +0600

    1070 working

---
+++ Uri-online-judge/1070.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    for (int i=a;i<a+12;i++)
+    {
+        if(i%2==1){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
