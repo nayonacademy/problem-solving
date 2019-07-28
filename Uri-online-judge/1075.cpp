fe04f798d178e9b71a8e8b4e438d7a822fc6ea8e
Shariful Islam <si.nayon@gmail.com>
2019-07-28 10:41:22 +0600

    solve 1075

---
+++ Uri-online-judge/1075.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long int a;
+    cin >> a;
+    for(int i = 2;i < 10000;i = i+a)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
