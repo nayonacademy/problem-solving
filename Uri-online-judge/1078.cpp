56b564f2fda979b8e65a23a9e6d6f17333252a2d
Shariful Islam <si.nayon@gmail.com>
2019-07-28 10:48:29 +0600

    1078 solve

---
+++ Uri-online-judge/1078.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    for (int i=1;i<11;i++)
+    {
+       cout << i <<" x "<< a <<" = "<< a * i << endl;
+    }
+    return 0;
+}
