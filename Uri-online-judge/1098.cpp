dc350463cfedcd85d462c91cd66352097f2eeefd
Shariful Islam <si.nayon@gmail.com>
2019-07-29 09:46:09 +0600

    1098 solve

---
+++ Uri-online-judge/1098.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for (double i=0.0;i<2.0;i=i+0.2){
+        cout << "I="<<0.0+i<<" J="<< 1.00+i << endl;
+        cout << "I="<<0.0+i<<" J="<< 2.00+i << endl;
+        cout << "I="<<0.0+i<<" J="<< 3.00+i << endl;
+    }
+     return 0;
+}
