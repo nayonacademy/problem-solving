12079b4baa571013b2db7896c36530a0bad3de0d
Shariful Islam <si.nayon@gmail.com>
2019-07-29 09:14:36 +0600

    1096 solve

---
+++ Uri-online-judge/1096.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<10;i=i+2){
+        cout << "I="<<i<<" J=7" << endl;
+        cout << "I="<<i<<" J=6" <<endl;
+        cout << "I="<<i<<" J=5" <<endl;
+    }
+    return 0;
+}
