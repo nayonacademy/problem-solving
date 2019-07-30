7ac2cef4343fec92e649fbf68af990c6beeff1f8
Shariful Islam <si.nayon@gmail.com>
2019-07-30 17:22:05 +0600

    1142 solve

---
+++ Uri-online-judge/1142.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=1;i<n*4;i=i+4){
+        cout << i << " " << i+1<< " " << i+2 <<" PUM" << endl;
+    }
+    return 0;
+}
