3ae918daec1322113eb06d44fc76f9fd29821a05
Shariful Islam <si.nayon@gmail.com>
2019-07-30 20:25:37 +0600

    1143 solve

--- Uri-online-judge/1143.cpp
+++ Uri-online-judge/1143.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
-    for(int i=1;)
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cout << i << " " << i * i << " " << i * i * i << endl;
+    }
     return 0;
 }
