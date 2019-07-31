ab22a31e5054c7e2cc778564de81b596e335fa76
Shariful Islam <si.nayon@gmail.com>
2019-07-31 15:05:57 +0600

    1157

---
+++ Uri-online-judge/1157.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    for (int i = 1; i<=a; i++)
+    {
+        if(a%i==0){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
