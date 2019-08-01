a399e21aa0a71e835b1d987c043040bdd93078cf
Shariful Islam <si.nayon@gmail.com>
2019-08-01 15:03:57 +0600

    1175 done

--- Uri-online-judge/1175.cpp
+++ Uri-online-judge/1175.cpp
@@ -3,14 +3,20 @@ using namespace std;
 
 int main()
 {
-    int N[20];
+    int N[20], temp;
     
-    for(int i=0; i<3; i++){
+    for(int i=0; i<20; i++){
         int a;
         cin >> a;
         N[i] = a;
     }
-
-    cout << N[0];
+    for(int i=0, j=19; i<10; i++, j--)
+    {
+        temp=N[i];
+        N[i]=N[j];
+        N[j]=temp;
+    }
+    for(int i=0; i<20; i++)
+        printf("N[%d] = %d\n",i,N[i]);
     return 0;
 }
